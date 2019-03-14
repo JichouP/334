c462a11669458739eecd2175d63ac762b169072c
JichouP <akkunn100@gmail.com>
2019-03-14 11:15:10 +0900

    done

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.h1``;
+
+const Counter = styled.h2``;
+
+const Button = styled.button``;
+
+interface Props {}
+interface State {
+  33: number;
+  4: number;
+}
+
+export default class App extends Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      33: 0,
+      4: 0,
+    };
+  }
+  onClickHandler = () => {
+    this.setState(prev => ({ 33: prev[33] + 33, 4: prev[4] + 4 }));
+  };
+  render() {
+    return (
+      <Container>
+        <Header>33-4 Counter</Header>
+        <Counter>{`${this.state[33]}-${this.state[4]}`}</Counter>
+        <Button onClick={this.onClickHandler}>Click!</Button>
+      </Container>
+    );
+  }
+}
